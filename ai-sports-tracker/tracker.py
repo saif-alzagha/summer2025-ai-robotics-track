5d747a749ebaec858c9367a2dce5fd716504879a
saif-alzagha <saifzagha2@gmail.com>
2025-06-22 22:21:17 -0400

    Color Contouring to detect a Basketball in a video

--- ai-sports-tracker/tracker.py
+++ ai-sports-tracker/tracker.py
@@ -3,7 +3,7 @@ import cv2
 
 print("running tracker.py")
 
-cap = cv2.VideoCapture("C:/Users/saifz/summer2025-ai-robotics-track/ai-sports-tracker/sample_input.mp4")
+cap = cv2.VideoCapture("C:/Users/saifz/summer2025-ai-robotics-track/ai-sports-tracker/sample_input2.mp4")
 
 if not cap.isOpened():
     print("Error: Could not open video file.")
@@ -20,9 +20,35 @@ while True:
     if not ret:
         break
 
-    # Display the frame
-    frame_resized = cv2.resize(frame, (800, 400))
+    # Resize the frame to fit the window
+    frame = cv2.resize(frame, (800, 400))
+    # Display the frame  
     cv2.imshow("Video", frame)
+
+    #converting BGR into HSV
+    hsv= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #define the color range in HSV for the basket ball
+    lower = (125, 8, 14)  # Lower bound for basketball color
+    upper = (170, 66, 38)  # Upper bound for basketballcolor
+
+    # Create a mask for the color
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    #loop through the contours and draw circles around the detected ball by filtering out small contours
+    for contour in contours:
+        if cv2.contourArea(contour) > 100:
+            # Find the minimum enclosing circle for the contour
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            # Draw the circle on the frame
+            cv2.circle(frame, center, radius, (0, 255, 0), 2)
+            #show mask
+            cv2.imshow("Mask", mask)
 
     # Exit on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
